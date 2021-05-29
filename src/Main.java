37e8546a148dfca52014a7fc38ff70e973490399
ARyabtsev79 <79724555+ARyabtsev79@users.noreply.github.com>
2021-05-29 11:55:16 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
         double height = 1.83; //рост в метрах
-        double weight = 101; //вес в килограммах
+        double weight = 101.2; //вес в килограммах
         double bodymassindex = service.calculate(height, weight);
         System.out.println("Ваш индекс массы тела: " + bodymassindex);
 
