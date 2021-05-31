e87c12648b88c61a4236a6a114dd1047c0501f49
ARyabtsev79 <79724555+ARyabtsev79@users.noreply.github.com>
2021-05-31 21:09:03 +0200

    converted result to double

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,8 @@
 public class BmiService {
 
-    public int calculate(double height, double weight) {
+    public double calculate(double height, double weight) {
         double bodymassindex;
         bodymassindex = weight / (height * height);
-        return (int) bodymassindex;
+        return bodymassindex;
     }
 }
